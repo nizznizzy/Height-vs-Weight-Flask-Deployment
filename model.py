c9c8e56730211a6ac8cbc23fa295765d160e7df1
Admin <307nizak@gmail.com>
2020-06-15 12:50:07 +0530

    modified

--- model.py
+++ model.py
@@ -24,4 +24,4 @@ pickle.dump(regressor, open('model.pkl','wb'))
 
 # Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[53.12]]))
+#print(model.predict([[53.12]]))
